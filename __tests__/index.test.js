5069df615d144e2249def0f75b4b1a036cc64ce2
sinashk78 <sina.sh.k78@gmail.com>
2020-11-14 17:25:22 +0330

    breaking a test to see what happens

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,7 +4,7 @@ const app = require("../app");
 it("should respond with Hello World!", async () => {
   const response = await request(app).get("/hello").send();
 
-  expect(response.text).toBe("Hello World!");
+  expect(response.text).toBe("Hello World!!!!!");
 });
 
 it("should respond with a status of 200", async () => {
